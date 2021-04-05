bee01db1ae6027f900a81969f3f99e1390701ddc
newbens <newbens@163.com>
2021-04-05 21:16:04 +0800

    第二次提交

--- src/main/java/com/niuben/download/domain/FileInfo.java
+++ src/main/java/com/niuben/download/domain/FileInfo.java
@@ -8,4 +8,20 @@ public class FileInfo {
         this.name = name;
         this.size = size;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
 }
