c0a8c12d5ef256d181f6d2f6f8bbfb3a4a861840
newbens <newbens@163.com>
2021-04-06 16:20:24 +0800

    断点上传bug修改

--- src/main/java/com/niuben/download/Controller/UploadController.java
+++ src/main/java/com/niuben/download/Controller/UploadController.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.BufferedOutputStream;
@@ -20,65 +19,63 @@ import java.util.List;
 @Controller
 public class UploadController {
 
-    private final static String UTF8 = "utf-8";
-
+    private final static String utf8 ="utf-8";
     @RequestMapping("/upload")
     @ResponseBody
-    public void upload(HttpServletRequest request,
-                         HttpServletResponse response) throws Exception{
-        response.setCharacterEncoding(UTF8);
-        Integer currentChunk = null; //当前分片数
-        Integer totalChunk = null; //总分片数
-        String name = null; //文件名称
-        String uploadPath = "C:\\blog-upload-download\\upload"; //存储路径
+    public void upload(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+        //分片
+        response.setCharacterEncoding(utf8);
+        Integer schunk = null;
+        Integer schunks = null;
+        String name = null;
+        String uploadPath = "C:\\blog-upload-download\\upload";
         BufferedOutputStream os = null;
-        try {
+        try{
             DiskFileItemFactory factory = new DiskFileItemFactory();
-            factory.setSizeThreshold(1024);//缓存区大小
-            factory.setRepository(new File(uploadPath));//存储路径
+            factory.setSizeThreshold(1024);
+            factory.setRepository(new File(uploadPath));
             ServletFileUpload upload = new ServletFileUpload(factory);
-            upload.setFileSizeMax(5l * 1024l * 1024l * 1024l);
-            upload.setSizeMax(10l * 1024l * 1024l * 1024l);
-            List<FileItem> fileItems = upload.parseRequest(request);
-            //获取文件信息
-            for (FileItem fileItem : fileItems) {
-                if (fileItem.isFormField()) {
-                    if ("chunk".equals(fileItem.getFieldName())) {
-                        currentChunk = Integer.parseInt(fileItem.getString(UTF8));
+            upload.setFileSizeMax(5l *1024l *1024l*1024l);
+            upload.setSizeMax(10l *1024l *1024l*1024l);
+            List<FileItem> items = upload.parseRequest(request);
+
+            for(FileItem item : items){
+                if(item.isFormField()){
+                    if("chunk".equals(item.getFieldName())){
+                        schunk = Integer.parseInt(item.getString(utf8));
                     }
-                    if ("chunks".equals(fileItem.getFieldName())) {
-                        totalChunk = Integer.parseInt(fileItem.getString(UTF8));
+                    if("chunks".equals(item.getFieldName())){
+                        schunks = Integer.parseInt(item.getString(utf8));
                     }
-                    if ("name".equals(fileItem.getFieldName())) {
-                        name = fileItem.getString(UTF8);
+                    if("name".equals(item.getFieldName())){
+                        name = item.getString(utf8);
                     }
                 }
             }
-            //获取文件分片
-            for (FileItem fileItem : fileItems) {
-                if (!fileItem.isFormField()) {
+            for(FileItem item : items){
+                if(!item.isFormField()){
                     String temFileName = name;
-                    if (name != null) {
-                        if (currentChunk != null) {
-                            temFileName = currentChunk+"-"+"name";
+                    if(name != null){
+                        if(schunk != null){
+                            temFileName = schunk +"_"+name;
                         }
                         File temFile = new File(uploadPath,temFileName);
-                        if(!temFile.exists()) {//如果文件不存在,就写入
-                            fileItem.write(temFile);
+                        if(!temFile.exists()){//断点续传
+                            item.write(temFile);
                         }
-
                     }
                 }
             }
             //文件合并
-            if (currentChunk != null && currentChunk.intValue() == totalChunk.intValue()-1) {  //说明有分片需要合并，并且是最后一个分片的话就需要合并。
-                File temFile = new File(uploadPath,name);
-                os = new BufferedOutputStream(new FileOutputStream(temFile));
-                //合并
-                for (int i = 0; i < totalChunk; i++) {
-                    File file = new File(uploadPath,i+"-"+name);
-                    while (file.exists()) { //判断分片是否存在，因为发送分片是一个并发的过程，最后一个分片到了，之前的分片不一定到了
-                        Thread.sleep(100);//休眠一会，等会继续获取
+            if(schunk != null && schunk.intValue() == schunks.intValue()-1){
+                File tempFile = new File(uploadPath,name);
+                os = new BufferedOutputStream(new FileOutputStream(tempFile));
+
+                for(int i=0 ;i<schunks;i++){
+                    File file = new File(uploadPath,i+"_"+name);
+                    while(!file.exists()){
+                        Thread.sleep(100);
                     }
                     byte[] bytes = FileUtils.readFileToByteArray(file);
                     os.write(bytes);
@@ -86,14 +83,14 @@ public class UploadController {
                     file.delete();
                 }
                 os.flush();
-                response.getWriter().write("上传成功"+name);
             }
+            response.getWriter().write("上传成功"+name);
         }finally {
-            try {
-                if (os != null) { //关闭流
+            try{
+                if(os != null){
                     os.close();
                 }
-            } catch (IOException e) {
+            }catch (IOException e){
                 e.printStackTrace();
             }
         }
